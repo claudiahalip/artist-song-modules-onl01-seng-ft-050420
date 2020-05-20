800da7677df9133af2abf219a40c134ca03b646e
claudia v <claudia4ro@yahoo.com>
2020-05-20 01:10:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
+  
+  extend Memorable
 
   @@songs = []
 
@@ -17,14 +19,7 @@ class Song
   def self.all
     @@songs
   end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  
 
   def artist=(artist)
     @artist = artist
